b8374413c584c5bf8815610e8f05879e7196438e
Maxence COEUR <maxence.coeur@etu.univ-lyon1.fr>
2024-12-12 18:45:09 +0100

    fix indentation

--- src/ControllerRequestDTO.cpp
+++ src/ControllerRequestDTO.cpp
@@ -21,37 +21,37 @@ cJSON* ControllerRequestDTO::toJson() const
 {
     cJSON* json = cJSON_CreateObject();
 
-        // Ajout conditionnel des joysticks
-        if (joystickLeft != nullptr) {cJSON_AddItemToObject(json, "joystickLeft", joystickLeft->toJson());} 
-        else {cJSON_AddNullToObject(json, "joystickLeft");}
+    // Ajout conditionnel des joysticks
+    if (joystickLeft != nullptr) {cJSON_AddItemToObject(json, "joystickLeft", joystickLeft->toJson());} 
+    else {cJSON_AddNullToObject(json, "joystickLeft");}
 
-        if (joystickRight != nullptr) {cJSON_AddItemToObject(json, "joystickRight", joystickRight->toJson());} 
-        else {cJSON_AddNullToObject(json, "joystickRight");}
+    if (joystickRight != nullptr) {cJSON_AddItemToObject(json, "joystickRight", joystickRight->toJson());} 
+    else {cJSON_AddNullToObject(json, "joystickRight");}
 
-        // Ajout du compteur
-        cJSON_AddNumberToObject(json, "counter", counter);
+    // Ajout du compteur
+    cJSON_AddNumberToObject(json, "counter", counter);
 
-        return json;
+    return json;
 }
 ControllerRequestDTO ControllerRequestDTO::fromJson(cJSON* json)
 {
     ControllerRequestDTO dto;
 
-        // Récupération de joystickLeft
-        cJSON* joystickLeftJson = cJSON_GetObjectItemCaseSensitive(json, "joystickLeft");
-        if (cJSON_IsObject(joystickLeftJson)) {dto.joystickLeft = new JoystickModel(JoystickModel::fromJson(joystickLeftJson));} 
-        else {dto.joystickLeft = nullptr;}
+    // Récupération de joystickLeft
+    cJSON* joystickLeftJson = cJSON_GetObjectItemCaseSensitive(json, "joystickLeft");
+    if (cJSON_IsObject(joystickLeftJson)) {dto.joystickLeft = new JoystickModel(JoystickModel::fromJson(joystickLeftJson));} 
+    else {dto.joystickLeft = nullptr;}
 
-        // Récupération de joystickRight
-        cJSON* joystickRightJson = cJSON_GetObjectItemCaseSensitive(json, "joystickRight");
-        if (cJSON_IsObject(joystickRightJson)) {dto.joystickRight = new JoystickModel(JoystickModel::fromJson(joystickRightJson));} 
-        else {dto.joystickRight = nullptr;}
+    // Récupération de joystickRight
+    cJSON* joystickRightJson = cJSON_GetObjectItemCaseSensitive(json, "joystickRight");
+    if (cJSON_IsObject(joystickRightJson)) {dto.joystickRight = new JoystickModel(JoystickModel::fromJson(joystickRightJson));} 
+    else {dto.joystickRight = nullptr;}
 
-        // Récupération du compteur
-        cJSON* counterJson = cJSON_GetObjectItemCaseSensitive(json, "counter");
-        if (cJSON_IsNumber(counterJson)) {
-            dto.counter = counterJson->valueint;
-        }
+    // Récupération du compteur
+    cJSON* counterJson = cJSON_GetObjectItemCaseSensitive(json, "counter");
+    if (cJSON_IsNumber(counterJson)) {
+        dto.counter = counterJson->valueint;
+    }
 
-        return dto;
+    return dto;
 };
