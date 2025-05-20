f967b082d52a78922bc77b99e29b08c887229c7d
Maxence COEUR <maxence.coeur@etu.univ-lyon1.fr>
2025-05-20 10:11:17 +0200

    delete toggle motorArming in addInControllerRequestDTO

--- src/ControllerRequestDTO.cpp
+++ src/ControllerRequestDTO.cpp
@@ -38,12 +38,9 @@ void ControllerRequestDTO::addInControllerRequestDTO(const ControllerRequestDTO 
         delete flightController; flightController = nullptr;
         flightController = new FlightController(*other.flightController);
     }
-    if (other.buttonMotorArming && *other.buttonMotorArming){
-        if (buttonMotorArming) {
-            *buttonMotorArming = !*buttonMotorArming;
-        } else {
-            buttonMotorArming = new bool(!*other.buttonMotorArming); // toggle à false
-        }
+    if(other.buttonMotorArming){
+        delete buttonMotorArming; buttonMotorArming = nullptr;
+        buttonMotorArming = new bool(*other.buttonMotorArming);
     }
     if(other.buttonMotorState){
         delete buttonMotorState; buttonMotorState = nullptr;
